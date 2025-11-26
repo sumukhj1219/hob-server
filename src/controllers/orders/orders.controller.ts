917de4327087bfa137be00c3a6f7792a61f4b6e0
sumukhj1219 <sumukhjoshi75@gmail.com>
2025-11-26 20:39:28 +0530

    added order routes

--- src/controllers/orders/orders.controller.ts
+++ src/controllers/orders/orders.controller.ts
@@ -65,3 +65,135 @@ export async function createOrder(req: Request, res: Response, next: NextFunctio
         next(error)
     }
 }
+
+export async function getPendingOrders(req: Request, res: Response, next: NextFunction) {
+    try {
+        const user = (req as any).user;
+        if (!user) throw new AppError("User not found", 404);
+
+        const userId = user.id as string;
+
+        const orderItems = await prisma.order.findMany({
+            where: {
+                userId: userId,
+                status: "PENDING"
+            },
+            include: {
+                items: {
+                    select: {
+                        product: true,
+                    }
+                }
+            }
+        })
+        return sendResponse(res, "Found pending orders", 200, orderItems)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export async function getDeliveredOrders(req: Request, res: Response, next: NextFunction) {
+    try {
+        const user = (req as any).user;
+        if (!user) throw new AppError("User not found", 404);
+
+        const userId = user.id as string;
+
+        const orderItems = await prisma.order.findMany({
+            where: {
+                userId: userId,
+                status: "DELIVERED"
+            },
+            include: {
+                items: {
+                    select: {
+                        product: true,
+                    }
+                }
+            }
+        })
+        return sendResponse(res, "Found delivered orders", 200, orderItems)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export async function getOrderStatus(req: Request, res: Response, next: NextFunction) {
+    try {
+        const user = (req as any).user;
+        if (!user) throw new AppError("User not found", 404);
+
+        const userId = user.id as string;
+        const orderId = req.params.id
+        if (!orderId) {
+            throw new AppError("Order Id not found", 400)
+        }
+
+        const orderStatus = await prisma.order.findUnique({
+            where: {
+                userId: userId,
+                id: orderId
+            },
+            select: {
+                status: true
+            }
+        })
+
+        return sendResponse(res, "Order status", 200, orderStatus)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export async function getAllOrders(req: Request, res: Response, next: NextFunction) {
+    try {
+        const user = (req as any).user;
+        if (!user) throw new AppError("User not found", 404);
+
+        const userId = user.id as string;
+
+        const orderItems = await prisma.order.findMany({
+            where: {
+                userId: userId,
+            },
+            include: {
+                items: {
+                    select: {
+                        product: true,
+                    }
+                }
+            }
+        })
+        return sendResponse(res, "Found all orders", 200, orderItems)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export async function cancelOrder(req: Request, res: Response, next: NextFunction){
+    try {
+        const user = (req as any).user;
+        if (!user) throw new AppError("User not found", 404);
+
+        const userId = user.id as string;
+        const orderId = req.params.id;
+
+        if(!orderId){
+            throw new AppError("Order Id not found", 400)
+        }
+
+        const cancelledOrder = await prisma.order.update({
+            where:{
+                userId:userId,
+                id: orderId
+            },
+            data:{
+                status:"CANCELLED"
+            }
+        })
+
+        return sendResponse(res, "Order cancelled", 200, cancelledOrder)
+    } catch (error) {
+        next(error)
+    }
+}
--- src/routers/orders/orders.router.ts
+++ src/routers/orders/orders.router.ts
@@ -1,9 +1,14 @@
 import express from "express"
 import { protect } from "../../middlewares/auth.middleware.js"
-import { createOrder } from "../../controllers/orders/orders.controller.js"
+import { cancelOrder, createOrder, getAllOrders, getDeliveredOrders, getOrderStatus, getPendingOrders } from "../../controllers/orders/orders.controller.js"
 
 const router = express.Router()
 
 router.post("/create", protect, createOrder)
+router.get("/getPendingOrders", protect, getPendingOrders)
+router.get("/getDeliveredOrders", protect, getDeliveredOrders)
+router.get("/getOrderStatus/:id", protect, getOrderStatus)
+router.get('/getAllOrders', protect, getAllOrders)
+router.post("/cancelOrder/:id", protect, cancelOrder)
 
 export default router
